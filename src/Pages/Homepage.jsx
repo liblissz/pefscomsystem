4297fe2617dc81e8eb0b7c9f358a1767d541c141
liblissz <liblissz3@gmail.com>
2025-07-06 00:28:35 -0700

    zz

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -5,7 +5,7 @@ import UseCases from '../Componennts/Usecases/Usecases'
 import Samples from '../Componennts/Samples/Samples'
 import Pricing from '../Componennts/Prising/Prising'
 import CTA from '../Componennts/CTA/CTA'
-import Team from '../Componennts/Team/Team'
+import Team from '../Pages/Team/Team'
 
 const Homepage = () => {
   return (
